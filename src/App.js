a1eb370f4b08a23e21450fe58e0d43a8e86e25d9
fulvio di maio <fulvio1989@gmail.com>
2023-08-23 17:04:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState} from "react";
 import './App.css';
 
 function App() {
+  const [count, SetCount] = useState(0);
+  const Increment = () => SetCount(prevCount => prevCount + 1);
+  const Decrement = () => SetCount(prevCount => prevCount - 1);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={Increment}>+</button>
+      <span>{count}</span>
+      <button onClick={Decrement}>-</button>
     </div>
   );
 }
